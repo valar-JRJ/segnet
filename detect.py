fad63e79d36f9f552465538f69143347bce5f002
valar_JRJ <1339856234>
2020-12-01 10:33:52 +0800

    detect

---
+++ detect.py
@@ -0,0 +1,182 @@
+from __future__ import print_function
+
+import argparse
+import glob
+import numpy as np
+from dataset import PascalVOCDataset, NUM_CLASSES
+from model import SegNet
+import os
+import time
+import datetime
+import torch
+import torch.optim
+from torch.utils.data import DataLoader, Dataset
+from torchvision import transforms
+import torch.nn.functional as F
+from PIL import Image
+import matplotlib.pyplot as plt
+
+
+def pad_to_square(img, pad_value):
+    c, h, w = img.shape
+    dim_diff = np.abs(h - w)
+    # (upper / left) padding and (lower / right) padding
+    pad1, pad2 = dim_diff // 2, dim_diff - dim_diff // 2
+    # Determine padding
+    pad = (0, 0, pad1, pad2) if h <= w else (pad1, pad2, 0, 0)
+    # Add padding
+    img = F.pad(img, pad, "constant", value=pad_value)
+
+    return img, pad
+
+
+def resize(image, size):
+    image = F.interpolate(image.unsqueeze(0), size=size, mode="nearest").squeeze(0)
+    return image
+
+
+def get_pascal_labels():
+    """Load the mapping that associates pascal classes with label colors
+
+    Returns:
+        np.ndarray with dimensions (21, 3)
+    """
+    return np.asarray(
+        [
+            [0, 0, 0],
+            [128, 0, 0],
+            [0, 128, 0],
+            [128, 128, 0],
+            [0, 0, 128],
+            [128, 0, 128],
+            [0, 128, 128],
+            [128, 128, 128],
+            [64, 0, 0],
+            [192, 0, 0],
+            [64, 128, 0],
+            [192, 128, 0],
+            [64, 0, 128],
+            [192, 0, 128],
+            [64, 128, 128],
+            [192, 128, 128],
+            [0, 64, 0],
+            [128, 64, 0],
+            [0, 192, 0],
+            [128, 192, 0],
+            [0, 64, 128],
+        ]
+    )
+
+
+def decode_segmap(label_mask, plot=False):
+    """Decode segmentation class labels into a color image
+
+    Args:
+        label_mask (np.ndarray): an (M,N) array of integer values denoting
+          the class label at each spatial location.
+        plot (bool, optional): whether to show the resulting color image
+          in a figure.
+
+    Returns:
+        (np.ndarray, optional): the resulting decoded color image.
+    """
+    label_colours = get_pascal_labels()
+    r = label_mask.copy()
+    g = label_mask.copy()
+    b = label_mask.copy()
+    for ll in range(0, NUM_CLASSES):
+        r[label_mask == ll] = label_colours[ll, 0]
+        g[label_mask == ll] = label_colours[ll, 1]
+        b[label_mask == ll] = label_colours[ll, 2]
+    rgb = np.zeros((label_mask.shape[0], label_mask.shape[1], 3))
+    rgb[:, :, 0] = r / 255.0
+    rgb[:, :, 1] = g / 255.0
+    rgb[:, :, 2] = b / 255.0
+    if plot:
+        plt.imshow(rgb)
+        plt.show()
+    else:
+        return rgb
+
+
+class ImageFolder(Dataset):
+    def __init__(self, folder_path, img_size=512):
+        self.files = sorted(glob.glob("%s/*.*" % folder_path))
+        self.img_size = img_size
+
+    def __getitem__(self, index):
+        img_path = self.files[index % len(self.files)]
+        # Extract image as PyTorch tensor
+        img = transforms.ToTensor()(Image.open(img_path))
+        # Pad to square resolution
+        img, _ = pad_to_square(img, 0)
+        # Resize
+        img = resize(img, self.img_size)
+
+        return img_path, img
+
+    def __len__(self):
+        return len(self.files)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--image_folder", type=str, default="data/samples", help="path to dataset")
+    parser.add_argument("--batch_size", type=int, default=1, help="size of the batches")
+    parser.add_argument("--img_size", type=int, default=512, help="size of each image dimension")
+    parser.add_argument("--checkpoint_model", type=str, help="path to checkpoint model")
+    opt = parser.parse_args()
+    print(opt)
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    os.makedirs("output", exist_ok=True)
+
+    model = SegNet().to(device)
+    model.load_state_dict(torch.load(opt.checkpoint_model))
+
+    model.eval()
+    dataloader = DataLoader(
+        ImageFolder(opt.image_folder, img_size=opt.img_size),
+        batch_size=opt.batch_size,
+        shuffle=False,
+        num_workers=4,
+    )
+
+    Tensor = torch.cuda.FloatTensor if torch.cuda.is_available() else torch.FloatTensor
+
+    imgs = []  # Stores image paths
+    img_detections = []  # Stores detections for each image index
+
+    print("\nPerforming object detection:")
+    prev_time = time.time()
+    for batch_i, (img_paths, input_imgs) in enumerate(dataloader):
+        # Configure input
+        input_imgs = torch.autograd.Variable(input_imgs.type(Tensor))
+
+        # Get detections
+        with torch.no_grad():
+            outputs = model(input_imgs)
+            pred = np.squeeze(outputs.data.max(1)[1].cpu().numpy(), axis=0)
+            decoded = decode_segmap(pred)    # return rgb
+            print("Classes found: ", np.unique(pred))
+
+        # Log progress
+        current_time = time.time()
+        inference_time = datetime.timedelta(seconds=current_time - prev_time)
+        prev_time = current_time
+        print("\t+ Batch %d, Inference Time: %s" % (batch_i, inference_time))
+
+        # Save image and detections
+        imgs.extend(img_paths)
+        img_detections.extend(decoded)
+
+    print("\nSaving images:")
+    # Iterate through images and save plot of detections
+    for img_i, (path, detections) in enumerate(zip(imgs, img_detections)):
+        print("(%d) Image: '%s'" % (img_i, path))
+
+        # Create plot
+        img = np.array(Image.open(path))
+        plt.figure()
+        fig, ax = plt.subplots(1)
+        ax.imshow(img)
